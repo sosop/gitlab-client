bb1cb6687ae51a0cc1c9258285111654bc36c89d
mac <mac@macdeMacBook-Pro-3.local>
2019-02-12 15:36:00 +0800

    修改project API

--- gitlabProject.go
+++ gitlabProject.go
@@ -2,8 +2,17 @@ package gitlabClient
 
 
 // 根据token判断所属项目
-func (gitlab *GitlabClient) ListOwnProject() ([]byte, error) {
+func (gitlab *GitlabClient) ListOwnProjects() ([]byte, error) {
 	data, err := gitlab.get("/api/v3/projects/owned", nil)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// 可访问的项目
+func (gitlab *GitlabClient) ListProjects() ([]byte, error) {
+	data, err := gitlab.get("/api/v3/projects", nil)
 	if err != nil {
 		return nil, err
 	}
--- gitlabProject_test.go
+++ gitlabProject_test.go
@@ -2,8 +2,16 @@ package gitlabClient
 
 import "testing"
 
-func TestGitLabClient_ListMyProject(t *testing.T) {
-	data, err := gitlab.ListOwnProject()
+func TestGitLabClient_ListOwnProjects(t *testing.T) {
+	data, err := gitlab.ListOwnProjects()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(data))
+}
+
+func TestGitLabClient_ListProjects(t *testing.T) {
+	data, err := gitlab.ListProjects()
 	if err != nil {
 		t.Fatal(err)
 	}
